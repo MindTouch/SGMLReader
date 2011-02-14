02369a98f8fd403529e8e0e7f01503a9e47a7eb5
Steve Bjorg <steve.bjorg@gmail.com>
2011-02-14 12:43:03 -0800

    added NUnit-based test assembly skeleton

---
+++ SGMLTests/HtmlTests.cs
@@ -0,0 +1,25 @@
+using System;
+using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+
+namespace SGMLTests {
+
+    [TestFixture]                    
+    public class UnitTests {
+
+        [Test]
+        public void First() {
+            Assert.Fail();
+        }
+
+        private string Read(string n) {
+            var assembly = typeof(UnitTests).Assembly;
+            var stream = assembly.GetManifestResourceStream(assembly.FullName.Split(',')[0] + "Resources." + n);
+            using(var sr = new StreamReader(stream)) {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
+
