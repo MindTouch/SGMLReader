5c01683f3eeb3056691a0ab2577566f01341ae8f
Steve Bjorg <steve.bjorg@gmail.com>
2011-06-12 23:09:24 -0700

    clarified license for code contributions

--- sgmlreaderdll/SgmlReader.cs
+++ sgmlreaderdll/SgmlReader.cs
@@ -6,9 +6,26 @@
  * based on SGML DTD information, and so on.
  *
  * Copyright (c) 2002 Microsoft Corporation. All rights reserved. (Chris Lovett)
- * 
+ *
+ */
+
+/*
  * Copyright (c) 2007-2011 MindTouch. All rights reserved.
- * 
+ * www.mindtouch.com  oss@mindtouch.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 using System;
