ac3bd8c5beaab9629d9a44bfb4a947594725f4ab
Corey Coto <corey.coto@gmail.com>
2015-11-04 15:24:19 -0800

    Bumped version number to 1.8.12

--- SgmlReader/AssemblyInfo.cs
+++ SgmlReader/AssemblyInfo.cs
@@ -50,8 +50,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.8.11")]
-[assembly: AssemblyFileVersion("1.8.11")]
+[assembly: AssemblyVersion("1.8.12")]
+[assembly: AssemblyFileVersion("1.8.12")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
